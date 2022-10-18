90bf9fcf929156ff82a7b29dcc14de815ce556b6
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-10-18 16:26:27 +0800

    feat(Portfolio): Base files for Part 3

---
+++ Portfolio-Part3/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms'),
+  ],
+}
