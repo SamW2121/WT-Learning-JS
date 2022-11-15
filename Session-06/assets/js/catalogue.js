019ac37c1c4612b26023b6c07cdc3b93e35fe359
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2022-11-15 12:29:21 +0800

    feat(session-07): Shopping Cart (Cart) - Update catalogue to add cart - Add to Cart now adds item to cart - Item qty will be 1 in this version - Clear cart clears the cart - Product costs now in INTEGERS     - This alleviates the problem if rounding - Costs shown to 2DP (Price/100).toFixed(2)

--- Session-06/assets/js/catalogue.js
+++ Session-06/assets/js/catalogue.js
@@ -1,10 +1,15 @@
-const wrapper = document.getElementById("catalogue")
+const catalogWrapper = document.getElementById("catalogue")
+const cartItems = document.getElementById("CartItems")
+const theTotalCost = document.getElementById("TotalCost")
 
-wrapper.innerHTML = ""
+// retrieve current cart if it exists. If it doesn't create an empty cart
+let cart = JSON.parse(localStorage.getItem("cart")) || []
 
+
+catalogWrapper.innerHTML = ""
 data.forEach((product) => {
 
-    // Create the card as an item wrapper
+    // Create the card as an item catalogWrapper
     let item = document.createElement("div")
     item.classList.add('border-zinc-900', 'border', 'rounded', 'flex', 'flex-col')
 
@@ -21,8 +26,8 @@ data.forEach((product) => {
     // Add image to photo section
     let itemImage = document.createElement('img')
     itemImage.classList.add("align-middle")
-    itemImage.src= `assets/images/${product.image}`
-    itemImage.alt=product.alt
+    itemImage.src = `assets/images/${product.image}`
+    itemImage.alt = product.alt
     // Add the image to the photo area, and add photo to card
     itemPhoto.appendChild(itemImage)
     item.appendChild(itemPhoto);
@@ -34,14 +39,15 @@ data.forEach((product) => {
     // create item price
     let itemPrice = document.createElement("p")
     itemPrice.classList.add("text-center", "bg-zinc-200", "p-2")
-    itemPrice.innerHTML = `$${product.price}`        // Add the price in form $xxxx.xx
+    // Add the price in form $xxxx.xx
+    itemPrice.innerHTML = `$${(product.price / 100).toFixed(2)}`
     itemFooter.appendChild(itemPrice)
 
     // Add the add to cart button
     let itemButton = document.createElement("button")
-    itemButton.classList.add("bg-blue-200", "border-blue-700", "p-2","rounded-b-md",
-                             "hover:bg-blue-700", "hover:text-white",
-                             "transition","duration-500")
+    itemButton.classList.add("bg-blue-200", "border-blue-700", "p-2", "rounded-b-md",
+        "hover:bg-blue-700", "hover:text-white",
+        "transition", "duration-500", "add-to-cart")
     itemButton.innerHTML = "Add to Cart"
 
     // Add info to the button which we will use
@@ -52,6 +58,70 @@ data.forEach((product) => {
 
     item.appendChild(itemFooter)
     // Add the Card to the catalogue
-    wrapper.appendChild(item)
+    catalogWrapper.appendChild(item)
     return true
 })
+
+Array.from(document.getElementsByClassName("add-to-cart"))
+    .forEach(function (cartButtonElement) {
+        cartButtonElement.addEventListener("click", (event) => {
+
+            let newItem = {
+                name: event.target.dataset.name,
+                price: event.target.dataset.price,
+                quantity: 1,
+            };
+
+            cart.push(newItem);
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            updateCart(cart)
+        });
+    });
+
+
+function updateCart(theCart) {
+    let overallPrice = 0
+    cartItems.innerHTML = ""
+
+    if (theCart.count === 0) {
+        theCart.innerHTML = '<div class="text-center p-2">Cart is Empty</div>\n'
+        theTotalCost.innerHTML = "$0.00"
+    }
+
+    if (theCart.length) {
+
+        theCart.forEach((cartItem) => {
+            overallPrice += parseInt(cartItem.price, 10);
+
+            // Add the item to the on screen cart
+            cartItems.innerHTML = `${cartItems.innerHTML}
+                <div class='grid grid-cols-12 gap-1 text-xs
+                            border border-l-0 border-t-0 border-r-0 border-b-1 border-b-zinc-400'>
+                    <p class='bg-red-900 text-white text-bold w-4  text-center'>&times;</p>
+                    <p class='col-span-6'>${cartItem.name}</p>
+                    <p class='text-right pr-1 border-r border-zinc-900  col-span-2'>${cartItem.quantity}</p>
+                    <p class='text-right col-span-3 pr-1'>$${intToDec2DP(cartItem.price)}</p>
+                </div>`
+        });
+        theTotalCost.innerHTML = `$${intToDec2DP(overallPrice)}`;
+    }
+
+}
+
+// adds an event listener to clear our cart
+document.getElementById("ClearCart").addEventListener("click", () => {
+    localStorage.removeItem("cart")
+    cart = []
+    cartItems.innerHTML = '<div class="text-center p-2">Cart is Empty</div>'
+    updateCart(cart)
+});
+
+function intToDec2DP(value) {
+    return (value / 100).toFixed(2)
+}
+
+updateCart(cart)
+
+
--- Session-06/assets/js/products.js
+++ Session-06/assets/js/products.js
@@ -1,42 +1,42 @@
 const data = [
         {
             name: "Lego Space Shuttle",
-            price: 299.99,
+            price: 29999,
             image: "10283.webp",
             alt: "Lego Space Shuttle- image from Lego.com - used for educational purposes only" ,
         },
 
         {
             name: "Galaxy S7 Tab Plus",
-            price: 1144.00,
+            price: 114400,
             image: "50072389_733738.webp",
             alt: "Galaxy S7 Tab Plus - used for educational purposes only" ,
         },
 
         {
             name: "Lego AT-AT &trade;",
-            price: 1299.99,
+            price: 129999,
             image: "75313_Prod.webp",
             alt: "Lego AT-AT - image from Lego.com - used for educational purposes only" ,
         },
 
         {
             name: "Lego Razor Crest &trade;",
-            price: 899.99,
+            price: 89999,
             image: "75331.webp",
             alt: "Razor Crest - image from Lego.com - used for educational purposes only" ,
         },
 
         {
             name: "Lego Minions &trade;",
-            price: 29.99,
+            price: 2999,
             image: "40420.webp",
             alt: "Minions - image from Lego.com - used for educational purposes only" ,
         },
 
         {
             name: "ADD THREE MORE PRODUCTS TO THIS LIST",
-            price: 29.99,
+            price: 2999,
             image: "40420.webp",
             alt: "PLUS IMAGES, PRICES and ALT TEXT",
         },
